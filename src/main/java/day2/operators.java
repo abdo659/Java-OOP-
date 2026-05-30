8027042effa53ffb55f298da4d39849f03848e2e
Abdelrahman Eltabaa <abdo.tabaa5@gmail.com>
2026-05-30 20:25:28 +0300

    1

---
+++ src/main/java/day2/operators.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class operators {
+   public static void main(String[] args){
+        int x =4;
+        int y=5 ;
+       System.out.println(x == y);
+       System.out.println(x != y);
+       System.out.println(x = y);
+
+       double d = 12.3d;
+       float f = 12.3f;
+       System.out.println(d==f);
+
+       double d1 = 12.3d;
+       int num1 = 12;
+       System.out.println(d1==num1);
+
+
+       double d2 = 12.0d;
+       int num2 = 12;
+       System.out.println(d2==num2);
+
+
+   }
+}
